9298d61ef1f7c46aa7b9044920a581696c3c4c78
Henry Emmanuel <ebongemmanuel197@gmail.com>
2026-02-18 09:38:09 +0100

    djdh

--- components/Telegram.tsx
+++ components/Telegram.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Telegram() {
     return (
         <div className="bg-[#CED8DD] py-[40px] md:py-[60px]">
-                <div className="border border-3 border-[#ffff] bg-[#FFFFFF1A] rounded-[30px] max-w-[1080px] w-[90%] mx-auto pt-[60px] pb-[40px]" style={{
+                <div className="border border-3 border-[#ffff] bg-[#FFFFFF1A] rounded-[30px] max-w-[1080px] w-[90%] mx-auto pt-[60px] pb-[60px]" style={{
                     backdropFilter: "blur(150px)"
                 }}>
                     <h2 className="text-center text-[28px] md:text-[32px] lg:text-[40px] font-bold text-black">Join Our Community Of Winning Traders</h2>
